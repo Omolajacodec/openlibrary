4af5d2399830ffd78e829709faac04c0ab8c9e8f
Christian Clauss <cclauss@me.com>
2020-03-10 13:34:39 +0100

    make_delete_query() should also return str

--- openlibrary/solr/update_work.py
+++ openlibrary/solr/update_work.py
@@ -113,7 +113,7 @@ def add_field(doc, name, value):
     Example:
 
     >>> tostring(add_field(Element("doc"), "foo", "bar"))
-    "<doc><field name="foo">bar</field></doc>"
+    b"<doc><field name="foo">bar</field></doc>"
 
     :param lxml.etree.ElementBase doc: Parent document to append to.
     :param str name:
@@ -134,7 +134,7 @@ def add_field_list(doc, name, field_list):
     Example:
 
     >>> tostring(add_field_list(Element("doc"), "foo", [1,2]))
-    "<doc><field name="foo">1</field><field name="foo">2</field></doc>"
+    b"<doc><field name="foo">1</field><field name="foo">2</field></doc>"
 
     :param lxml.etree.ElementBase doc: Parent document to append to.
     :param str name:
@@ -1183,7 +1183,7 @@ def make_delete_query(keys):
     for key in keys:
         query = SubElement(delete_query,'query')
         query.text = 'key:%s' % key
-    return tostring(delete_query)
+    return tostring(delete_query).decode('utf-8')
 
 def update_author(akey, a=None, handle_redirects=True):
     """
